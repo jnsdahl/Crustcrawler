7f3204efb1af5c68c6dfdc94f637437673849c84
ubuntu_12.04_ROS <ubuntu@ubuntu.(none)>
2015-10-07 05:34:41 -0700

    stuff works better.

--- nodes/au_dynamixel_invkin_test_node.py
+++ nodes/au_dynamixel_invkin_test_node.py
@@ -2,6 +2,7 @@
 import rospy
 import actionlib
 import numpy as np
+from block_detection import get_blocks
 from std_msgs.msg import Float64
 from control_msgs.msg import FollowJointTrajectoryAction
 from control_msgs.msg import FollowJointTrajectoryFeedback
@@ -11,56 +12,56 @@ from trajectory_msgs.msg import JointTrajectoryPoint
 from trajectory_msgs.msg import JointTrajectory
 from math import atan2, cos, sin, sqrt, pi
 
+
 def transformation(xyz):
-	xc = xyz[0];
-	yc = xyz[1];
-	zc = -10;
+    xc = xyz[0] / 9
+    yc = xyz[1] / 9
+    zc = -10
 
-	my_point = np.array([xc, yc, zc, 1])
-	my_point = my_point[:, None]
-	print my_point
+    my_point = np.array([xc, yc, zc, 1])
+    my_point = my_point[:, None]
 
-	tx = -34;
-	ty = 35;
-	tz = 0;
-	thetax = pi;
-	thetaz = pi/2;
-	
-	A = np.matrix([[1, 0, 0, tx],[0, cos(thetax), -sin(thetax), ty],[0, sin(thetax), cos(thetax), tz],[0, 0, 0, 1]])
-	B = np.matrix([[cos(thetaz), sin(thetaz), 0, 0],[-sin(thetaz), cos(thetaz), 0, 0],[0, 0, 1, 0],[0, 0, 0, 1]])
+    tx = -34
+    ty = 35
+    tz = 0
+    thetax = pi
+    thetaz = pi / 2
 
-	my_Newpoint = A.dot(my_point)
-	my_Newpoint = B.dot(my_Newpoint)
-	print my_Newpoint
+    A = np.matrix([[1, 0, 0, tx], [0, cos(thetax), -sin(thetax), ty], [0, sin(thetax), cos(thetax), tz], [0, 0, 0, 1]])
+    B = np.matrix([[cos(thetaz), sin(thetaz), 0, 0], [-sin(thetaz), cos(thetaz), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 
-	return my_Newpoint[0], my_Newpoint[1], my_Newpoint[2]
+    my_Newpoint = A.dot(my_point)
+    my_Newpoint = B.dot(my_Newpoint)
+
+    return my_Newpoint[0], my_Newpoint[1], my_Newpoint[2]
+
 
 def gripper(value):
-	grip_pub = rospy.Publisher('/gripper/command', Float64)
-	grip_pub.publish(Float64(value))
+    grip_pub = rospy.Publisher('/gripper/command', Float64)
+    grip_pub.publish(Float64(value))
 
-def findAngle(vector):
-	x1 = vector[0][0]/9
-	x2 = vector[1][0]/9
-	y1 = vector[0][1]/9
-	y2 = vector[1][1]/9
 
-	xy1_new = transformation([x1, y1, 0])#[x1, y1]
-	xy2_new = transformation([x2, y2, 0])#[x2, y2]
-	
-	xv = xy1_new[0]-xy2_new[0]
-	yv = xy1_new[1]-xy2_new[1]
-	print 'det her er xv     ', xv, '     det her er yv     ', yv	
+def findAngle(point1, point2):
 
-	theta = Float64(atan2(yv, xv))
-	print'theta: ', theta
+    x1 = point1[0]
+    y1 = point1[1]
 
-	return theta
+    x2 = point2[0]
+    y2 = point2[1]
 
-	
+    xy1_new = transformation([x1, y1, 10])
+    xy2_new = transformation([x2, y2, 10])
 
-def invkin(xyz, theta = 0):
-	"""
+    xv = xy1_new[0] - xy2_new[0]
+    yv = xy1_new[1] - xy2_new[1]
+
+    theta = atan2(yv, xv)
+
+    return theta
+
+
+def invkin(xyz, theta):
+    """
 	Python implementation of the the inverse kinematics for the crustcrawler
 	Input: xyz position
 	Output: Angels for each joint: q1,q2,q3,q4
@@ -69,92 +70,97 @@ def invkin(xyz, theta = 0):
 	The robot model shown in rviz can be adjusted accordingly by editing au_crustcrawler_ax12.urdf
 	"""
 
-	d1 = 10.0; # cm (height of 2nd joint)
-	a1 = 0.0; # (distance along "y-axis" to 2nd joint)
-	a2 = 20.0; # (distance between 2nd and 3rd joints)
-	d4 = 20.0; # (distance from 3rd joint to gripper center - all inclusive, ie. also 4th joint)
+    d1 = 10.0  # cm (height of 2nd joint)
+    a1 = 0.0  # (distance along "y-axis" to 2nd joint)
+    a2 = 20.0  # (distance between 2nd and 3rd joints)
+    d4 = 20.0  # (distance from 3rd joint to gripper center - all inclusive, ie. also 4th joint)
 
-	# Insert code here!!!
-	xc = xyz[0];
-	yc = xyz[1];
-	zc = xyz[2];
-	
-	
-	q1 = atan2(yc, xc);
+    # Insert code here!!!
+    xc = xyz[0]
+    yc = xyz[1]
+    zc = xyz[2]
 
-	# calculation of q2 and q3
-	r2 = (xc - a1* cos(q1))**2+(yc - a1* sin(q1))**2;
-	s = (zc - d1);
-	D = (r2 + s**2 - a2**2 - d4**2)/(2 * a2 * d4);
-	
-	q3 = atan2(-sqrt(1-D**2), D);
-	
-	q2 = atan2(s, sqrt(r2)) - atan2(d4 * sin(q3), a2 + d4* cos(q3));
-	
-	#calculation of q4
-			
-	q4 = 0;
-	
-	
+    q1 = atan2(yc, xc)
 
-	return q1, q2-pi/2 , q3, q4
+    # calculation of q2 and q3
+    r2 = (xc - a1 * cos(q1)) ** 2 + (yc - a1 * sin(q1)) ** 2
+    s = (zc - d1)
+    D = (r2 + s ** 2 - a2 ** 2 - d4 ** 2) / (2 * a2 * d4)
 
+    q3 = atan2(-sqrt(1 - D ** 2), D)
+
+    q2 = atan2(s, sqrt(r2)) - atan2(d4 * sin(q3), a2 + d4 * cos(q3))
+
+    q4 = theta - q1
+
+    return q1, q2 - pi / 2, q3, q4
 
 
 class ActionExampleNode:
+    N_JOINTS = 4
 
-	N_JOINTS = 4
-	def __init__(self,server_name):
-		
-		self.client = actionlib.SimpleActionClient(server_name, FollowJointTrajectoryAction)
+    def __init__(self, server_name):
 
-		self.joint_positions = []
-		self.names =["joint1",
-				"joint2",
-				"joint3",
-				"joint4"
-				]
-		# the list of xyz points we want to plan
-		xyz_positions = [
-		[40, 15, 10]
-		]
-		
-		#findAngle q4
-		findAngle([[100, 100], [200, 200]])
+        self.client = actionlib.SimpleActionClient(server_name, FollowJointTrajectoryAction)
 
-		#Transformation
-		xyz_newpositions = []
-	
-		for i in xyz_positions:
-			xyz_newpositions.append(transformation(i))
-			
-		
-		# initial duration
-		dur = rospy.Duration(1)
+        self.joint_positions = []
+        self.names = ["joint1",
+                      "joint2",
+                      "joint3",
+                      "joint4"]
 
-		# construct a list of joint positions by calling invkin for each xyz point
-		for p in xyz_newpositions:
-			jtp = JointTrajectoryPoint(positions=invkin(p),velocities=[0.5]*self.N_JOINTS ,time_from_start=dur)
-			dur += rospy.Duration(2)
-			self.joint_positions.append(jtp)
+        blocks = get_blocks()
+        block = blocks[0]
 
-		self.jt = JointTrajectory(joint_names=self.names, points=self.joint_positions)
-		self.goal = FollowJointTrajectoryGoal( trajectory=self.jt, goal_time_tolerance=dur+rospy.Duration(2) )
-		
-		
-		gripper(0)
-		
-	def send_command(self):
-		self.client.wait_for_server()
-		print self.goal
-		self.client.send_goal(self.goal)
+        # the list of xyz points we want to plan
+        xyz_positions = [
+            [
+                block[2][0] + (block[0][0] - block[2][0]) / 2,
+                block[3][1] + (block[1][1] - block[3][1]) / 2,
+                10
+            ]
+        ]
 
-		self.client.wait_for_result()
-		print self.client.get_result()
+        # findAngle q4
+        t1 = findAngle(block[0], block[1])
+        t2 = findAngle(block[1], block[2])
+        theta = t1 if t1 < t2 else t2
+
+        print theta
+
+        # Transformation
+        xyz_newpositions = []
+
+        for i in xyz_positions:
+            xyz_newpositions.append(transformation(i))
+
+
+        # initial duration
+        dur = rospy.Duration(1)
+
+        # construct a list of joint positions by calling invkin for each xyz point
+        for p in xyz_newpositions:
+            jtp = JointTrajectoryPoint(positions=invkin(p, theta), velocities=[0.5] * self.N_JOINTS, time_from_start=dur)
+            dur += rospy.Duration(2)
+            self.joint_positions.append(jtp)
+
+        self.jt = JointTrajectory(joint_names=self.names, points=self.joint_positions)
+        self.goal = FollowJointTrajectoryGoal(trajectory=self.jt, goal_time_tolerance=dur + rospy.Duration(2))
+
+        gripper(0)
+
+    def send_command(self):
+        self.client.wait_for_server()
+        #print self.goal
+        self.client.send_goal(self.goal)
+
+        self.client.wait_for_result()
+        #print self.client.get_result()
+
 
 if __name__ == "__main__":
-	rospy.init_node("au_dynamixel_test_node")
+    rospy.init_node("au_dynamixel_test_node")
 
-	node= ActionExampleNode("/arm_controller/follow_joint_trajectory")
+    node = ActionExampleNode("/arm_controller/follow_joint_trajectory")
 
-	node.send_command()
+    node.send_command()
---
+++ nodes/block_detection.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+import cv2
+import numpy as np
+import urllib
+
+DEBUG = False
+if __name__ == "__main__":
+    DEBUG = True
+
+
+def draw_contours(contours, hierarchy, image, parent=-1, level=0):
+    colors = [
+        (0,0,255),
+        (255,0,0),
+        (0,255,0),
+    ]
+
+    for i in range(0, len(contours)):
+        # iterate parents children only
+        if hierarchy[i, 3] != parent:
+            continue
+
+        cv2.drawContours(image, [contours[i]], -1, colors[level], 2)
+
+        if hierarchy[i, 2] != -1:
+            draw_contours(contours, hierarchy, image, i, level+1)
+
+
+# Returns a block if a contour qualifies
+def block_from_contour(contour):
+    epsilon = 0.1 * cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, epsilon, True)
+    rect = cv2.minAreaRect(approx)
+    block = cv2.cv.BoxPoints(rect)
+    block = np.int0(block)
+    area = cv2.contourArea(approx)
+    width = np.linalg.norm(block[1] - block[0])
+    height = np.linalg.norm(block[1] - block[2])
+    wh_ratio = width/height if width/height <= 1 else height/width
+
+    if 800 < area < 2500 and 10 < epsilon < 22 and wh_ratio > 0.6:
+        return block
+    else:
+        return []
+
+
+# recursivly find blocks from contour tree.
+def blocks_from_contour_tree(contours, hierarchy, parent=-1):
+    blocks = []
+
+    # iterate children of parent
+    for i in range(0, len(contours)):
+        if hierarchy[i, 3] != parent:
+            continue
+
+        children_blocks = []
+
+        if hierarchy[i, 2] != -1:
+            children_blocks = blocks_from_contour_tree(contours, hierarchy, i)
+
+        if len(children_blocks) > 0:
+                blocks.extend(children_blocks)
+        else:
+            block = block_from_contour(contours[i])
+            if len(block) > 0:
+                blocks.append(block)
+
+    return blocks
+
+
+def preprocess(img):
+    # Crop to table
+    img = img[100:440, 30:635]
+
+    # Hide robot
+    cv2.rectangle(img, (220, 210), (380, 340), 0, -1)
+
+    return img
+
+
+def find_blocks(image):
+    img = image
+
+    # Smoothen image
+    img = cv2.adaptiveBilateralFilter(img, (9, 9), 100)
+
+    # Sharpen image with laplacian
+    lp = cv2.Laplacian(img, cv2.CV_8U)
+
+    ret, lp = cv2.threshold(lp, 10, 255, cv2.THRESH_BINARY)
+    img += 12*lp
+
+    # Detect edges
+    img = cv2.Canny(img, 35, 100)
+
+    # Dilate edges
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    img = cv2.dilate(img, kernel, iterations=3)
+
+    # Close edges
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+
+    # Erode edges
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Find contours
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Draw contours
+    draw_contours(contours, hierarchy[0], image)
+
+    # Find blocks from contours
+    blocks = blocks_from_contour_tree(contours, hierarchy[0])
+
+    return blocks
+
+
+def get_from_webcam():
+    stream = urllib.urlopen('http://192.168.0.20/image/jpeg.cgi')
+    bytes = ''
+    bytes += stream.read(64500)
+    a = bytes.find('\xff\xd8')
+    b = bytes.find('\xff\xd9')
+
+    if a != -1 and b != -1:
+        jpg = bytes[a:b+2]
+        return cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
+
+
+def get_blocks():
+    img = get_from_webcam()
+    img = preprocess(img)
+
+    return find_blocks(img)
